ec25ab8f151320a58c639049253dd1e96882de91
SatriaAkbarRizki <sartiapl@gmail.com>
2026-01-19 13:12:26 +0700

    Add: showmodal bottomsheet and animation loading

---
+++ lib/widget/home/loadingresult.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
+
+class Loadingresult extends StatelessWidget {
+  const Loadingresult({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 10,
+      children: [
+        Shimmer(
+          duration: Duration(seconds: 3),
+
+          child: Container(
+            height: 15,
+            width: 150,
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+        Shimmer(
+          duration: Duration(seconds: 3),
+
+          child: Container(
+            height: 15,
+            width: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+        Shimmer(
+          duration: Duration(seconds: 3),
+
+          child: Container(
+            height: 15,
+            width: 80,
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+        Shimmer(
+          duration: Duration(seconds: 3),
+
+          child: Container(
+            height: 15,
+            width: 180,
+            decoration: BoxDecoration(
+              color: Colors.grey[400],
+              borderRadius: BorderRadius.circular(5),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
